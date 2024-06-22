dda84ef0fa97312f562cf60204b19842881f1cb1
BitHero <nuhumusah874@gmail.com>
2024-06-22 22:21:30 +0000

    Field component to deal with state at the field level

--- src/Field.js
+++ src/Field.js
@@ -9,10 +9,33 @@ class Field extends Component {
     validate: PropTypes.func,
     onchange: PropTypes.func.isRequired
   }
+
+  state = {
+    value: this.props.value,
+    errors: false
+  }
+
+  getDerivedStateFromProps(nextProps) {
+    return {value: nextProps.value}
+  }
+
+  onChange = evt => {
+    const name = this.props.name
+    const value = evt.target.value
+    const error = this.props.validate ? this.props.validate(value) : false
+
+    this.setState({ value, error })
+
+    this.props.onChange({name, value, error})
+  }
   render() {
     return (
       <div>
-        
+        <input
+          placeholder={this.props.placeholder}
+          value={this.state.value}
+          onChange={this.onChange} 
+        />
       </div>
     );
   }
